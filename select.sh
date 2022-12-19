0e06231bcfabd1f936f25b1af22c741a20786296
nadaezzat-99 <nadaezzat507@gmail.com>
2022-12-19 22:47:13 +0200

    select

---
+++ select.sh
@@ -0,0 +1,36 @@
+select tables in `ls $DB_DIR/$dbname` "Exit"
+do 
+
+  if [ $REPLY -gt $databases_num ]
+  then 
+       exit
+
+  else [ $REPLY -lt $databases_num ] || [ $REPLY -eq $databases_num ]
+
+      table_name=`ls $DB_DIR/$dbname/ | head -$REPLY`
+      echo $table_name
+
+      typeset -i col_num
+      col_num=`head -1 $DB_DIR/$dbname/$table_name`
+      
+      columns=`tail +2 $DB_DIR/$dbname/$table_name | head -1 `
+      IFS=:
+      select field in $columns Exit
+      do 
+            if ! [ $REPLY -gt  $col_num ]
+            then    
+                echo $field
+                echo $REPLY
+                echo Delete from $table_name where $field = 
+                read value
+                selected_rows=`awk -F ':' -v FIELD=$REPLY -v DATA=$value '{if($FIELD == DATA) print}' $DB_DIR/$dbname/$table_name `
+                echo $selected_rows
+            else
+                break;
+            fi
+      done
+    fi
+done
+
+
+
