5b417d9cbe62465fb3f1516b0871b24dd265e549
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2022-12-20 21:04:05 +0200

    list databases

--- project.sh
+++ project.sh
@@ -5,71 +5,69 @@ DB_DIR=$WORKING_DIR/DBM
 
 # creating DBM if doesn't exist
 mkdir -p $DB_DIR
-
 while true
 do
+	echo
 	select choice in "Create_Database" "List_Databases" "Connect_Database" "Drop_Database" "Exit"
     	do 
 		case $REPLY in 
 	
-		1) echo enter database name 
+		1)  echo "Enter new database name:"
 			read dbname
-			if ! [[ $dbname =~ ^[a-zA-Z_0-9]+$ ]]
+			if ! [[ $dbname =~ ^[a-zA-Z_][a-zA-Z_0-9]+$ ]]
 			then
-				echo Avoid special characters
-				break;
+				echo "Avoid using special characters"
 			else
-            	check_dbname=`ls $DB_DIR/ | grep ^$dbdbname$`
-            	echo $check_dbname
-                
-				if [ $check_dbname ]
+				if [ -d $DB_DIR/$dbname ]
 				then
-					echo database $dbname exists
-		            break;
+					echo "database $dbname already exists."
                 else
                     mkdir $DB_DIR/$dbname
-					echo $dbname database is created
-		            break;	
+					echo "$dbname database is created."
                 fi 
-		   fi
-	    ;;
-          	
-	    2) ls -F $DB_DIR | grep "/"
+			fi
+		    break;;
+
+	    2) ls -F $DB_DIR | grep "/" | cut -f1 -d'/'
 		   break;
             ;;
 		  
-	    3) echo Enter database name to connect
-		   	read dbname
+		3)  echo "This is all databases: "
+			ls -F $DB_DIR | grep "/" | cut -f1 -d'/'
+			echo "Enter database name to connect"
+			read dbname
             check_dbname=`ls $DB_DIR/ | grep ^$dbname$`
 		   	if [ -z $check_dbname ]
             then
-                echo Invalid database name
+                echo "database $dbname doesnot exist"
 				break;
 		   	else	   
 		        cd $DB_DIR/$dbname
-		        echo you are connected to the $dbname database
+		        echo "you are connected to the $dbname database"
 				source $WORKING_DIR/tables.sh
 		   	fi
 		;;
 
-	    4) echo Enter database name to drop 
+	    4)	echo "This is all databases: " 
+			ls -F $DB_DIR | grep "/" | cut -f1 -d'/'
+			echo "Enter database name to drop" 
 		   	read dbname
 		   	check_dbname=`ls $DB_DIR/ | grep ^$dbname$`
             if [ -z $check_dbname ]
             then
-                echo database $dbname doesnot not exist
+                echo "database $dbname doesnot exist"
 				break;
     	    else
 		        rm -r $DB_DIR/$dbname
-		        echo  $dbname database is dropped 
+		        echo  "$dbname database is dropped"
 				break;
 		   	fi
 		;;
 		 
-	    5) exit
+	    5)  exit
 		;;	
 
-		*) echo $REPLY is not one of the choices.
+		*)  echo $REPLY is not one of the choices.
             echo Try again
 			break;
 		;;
